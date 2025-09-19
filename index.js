952a9dfef0764c120b5237d7de65cba81defae21
Romy-Mukendi <70291492+Romy-Mukendi@users.noreply.github.com>
2025-09-18 23:59:59 -0600

    images

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
-document.querySelector("button").addEventListener("click", handleClick);
+for(var i = 0; i<document.querySelectorAll(".drum").length; i++){
 
-function handleClick(){
-    alert("I got clicked");
+    document.querySelectorAll("button")[i].addEventListener("click", handleClick);
+
+    function handleClick(){
+        const audio1 = new Audio("/sounds/tom-1.mp3")
+        return audio1.play();
+    }
 }
