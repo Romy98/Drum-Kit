5cadb7d8826426b9ece73e96cd6036cc10dd6d27
Romy-Mukendi <70291492+Romy-Mukendi@users.noreply.github.com>
2025-09-23 01:35:36 -0600

    Added sounds for keyboard keys pressed.

--- index.js
+++ index.js
@@ -42,3 +42,39 @@ for(var i = 0; i<document.querySelectorAll(".drum").length; i++){
     }
 }
 
+document.addEventListener("keydown", function (event){
+    let keyPressed = event.key;
+    switch (keyPressed) {
+            case "w":
+                const tom1 = new Audio("/sounds/tom-1.mp3")
+                return tom1.play();
+            break;
+            case "a":
+                const tom2 = new Audio("/sounds/tom-2.mp3")
+                return tom2.play();
+            break;
+            case "s":
+                const tom3 = new Audio("/sounds/tom-3.mp3")
+                return tom3.play();
+            break;
+            case "d":
+                const tom4 = new Audio("/sounds/tom-4.mp3")
+                return tom4.play();
+            break;
+            case "j":
+                const snare = new Audio("/sounds/snare.mp3")
+                return snare.play();
+            break;
+            case "k":
+                const crash = new Audio("/sounds/crash.mp3")
+                return crash.play();
+            break;
+            case "l":
+                const kick = new Audio("/sounds/kick-bass.mp3")
+                return kick.play();
+            break;
+            default:
+                console.log(buttonInnerHTML);
+                break;
+        }
+})
