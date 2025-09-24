41f63235988ba38e6c3dd4d67daa6bd425df56cd
Romy-Mukendi <70291492+Romy-Mukendi@users.noreply.github.com>
2025-09-24 05:31:54 -0600

    cleaned up the code making a function for the sounds.

--- index.js
+++ index.js
@@ -4,47 +4,19 @@ for(var i = 0; i<document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll("button")[i].addEventListener("click", handleClick);
 
     function handleClick(){
-
         let buttonInnerHTML = this.innerHTML;
-        switch (buttonInnerHTML) {
-            case "w":
-                const tom1 = new Audio("/sounds/tom-1.mp3")
-                return tom1.play();
-            break;
-            case "a":
-                const tom2 = new Audio("/sounds/tom-2.mp3")
-                return tom2.play();
-            break;
-            case "s":
-                const tom3 = new Audio("/sounds/tom-3.mp3")
-                return tom3.play();
-            break;
-            case "d":
-                const tom4 = new Audio("/sounds/tom-4.mp3")
-                return tom4.play();
-            break;
-            case "j":
-                const snare = new Audio("/sounds/snare.mp3")
-                return snare.play();
-            break;
-            case "k":
-                const crash = new Audio("/sounds/crash.mp3")
-                return crash.play();
-            break;
-            case "l":
-                const kick = new Audio("/sounds/kick-bass.mp3")
-                return kick.play();
-            break;
-            default:
-                console.log(buttonInnerHTML);
-                break;
-        }
+        makeSounds(buttonInnerHTML);
     }
 }
 
 document.addEventListener("keydown", function (event){
     let keyPressed = event.key;
-    switch (keyPressed) {
+    makeSounds(keyPressed);
+})
+
+
+function makeSounds(key){
+    switch (key) {
             case "w":
                 const tom1 = new Audio("/sounds/tom-1.mp3")
                 return tom1.play();
@@ -77,4 +49,4 @@ document.addEventListener("keydown", function (event){
                 console.log(buttonInnerHTML);
                 break;
         }
-})
+}
